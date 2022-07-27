c1ad7ce7ef713830943a83e581a0438f00cb072e
Sanket Patil <san162002@gmail.com>
2022-07-27 16:55:12 +0530

    updated login page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 
 const app = express();
 
-app.use(express.urlencoded());
+//app.use(express.urlencoded());
 app.use(express.static('public'))
 app.set('view engine','ejs')
 
